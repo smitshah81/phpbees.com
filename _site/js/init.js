0a851ba7696149a7abdd1321b3b2bfb9946a0fc6
smit <smitshah81@gmail.com>
2017-09-09 18:38:59 +0000

    compiled pages added

---
+++ _site/js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.carousel').carousel();
+    $(".progress").hide();
+    
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
